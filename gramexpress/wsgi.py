ae88818bddd69aeb45db787bdbb33f377308e242
KshamayBharadwaj <kshamaypbharadwaj@gmail.com>
2026-04-05 15:37:13 +0000

    changed the WSGI file

--- gramexpress/wsgi.py
+++ gramexpress/wsgi.py
@@ -6,11 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+path = '/home/yourusername/YOUR_REPO'
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gramexpress.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
